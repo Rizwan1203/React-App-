d3739412204ce4e6f8541e837f2f4b6b99c17641
Rizwan <Rizwan1203@gmail.com>
2024-01-09 19:05:14 +0500

    build fix

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,36 +1,7 @@
 import { useMutation, useQuery } from "react-query";
-import { askQuestion, uploadCSV } from "../util/api";
 import toast from "react-hot-toast";
 import AuthService, { SignInFormData, SignUpFormData } from "../util/services/auth.service";
 import UserService, { AddItemData, Item } from "../util/services/item.service";
-
-
-
-export const useUploadCSV = (onSuccess: () => void) => useMutation({
-    mutationFn:  (formData: FormData) => uploadCSV(formData),
-    onSuccess: () => {
-        onSuccess();
-        toast.success("File Uploaded Successfully")
-    },
-    onError: () => {
-        onSuccess();
-        toast.error("Something went wrong!")
-    }
-
-
-  }) 
-
-export const useAskQuestion = (onSuccess: (data: any) => void) => useMutation({
-    mutationFn:  (query: string) => askQuestion(query),
-    onSuccess: (data) => {
-        onSuccess(data);
-    },
-    onError: () => {
-        toast.error("Something went wrong!")
-    }
-
-
-  }) 
 
 
 export const useRegister = (onSuccess: () => void) => useMutation({
